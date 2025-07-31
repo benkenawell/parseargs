293ac543a6d11d5e561c3b98f0b7f7f59562d8f9
Benjamin Kenawell <ben@benkenawell.com>
2025-07-31 18:40:23 -0400

    add short option to --option flag
    add some validation to option parsing

--- index.js
+++ index.js
@@ -3,25 +3,36 @@
 if (process.argv.length === 2) {
   const { basename } = await import('node:path');
   process.stdout.write(`
+USAGE
 
-Use "--" to separate arguments to this command from arguments you want to parse.
-
---config <string>     JSON config to pass to nodejs parseArgs. Overridden by other options.
---option <key>=<type> easy way to set an option key for parsing. Can be "string" or "boolean"
---[no-]positional     whether to allow positionals or not
---[no-]negative       whether to allow negatives or not
---[no-]strict         whether to set the config to strict or not.
+  Use "--" to separate arguments to this command from arguments you want to parse.
+  
+  --config <string>             JSON config to pass to nodejs parseArgs. Overridden by other options.
+  --option <key>=<type>         easy way to set an option key for parsing. type can be "string" or "boolean"
+  --option <key>=<type>,<short> easy way to set an option key for parsing.
+                                type can be "string" or "boolean". 
+                                short must be a single letter
+  --[no-]positional             whether to allow positionals or not
+  --[no-]negative               whether to allow negatives or not
+  --[no-]strict                 whether to set the config to strict or not.
 
 Config Reference
 
-If you'd like, you can write a json config
-that conforms to the NodeJS parseArgs library. The other command line options take precedence over --config
-Pass the options you want parsed as other arguments, after a "--".
+  If you'd like, you can write a json config
+  that conforms to the NodeJS parseArgs library. The other command line options take precedence over --config
+  Pass the options you want parsed as other arguments, after a "--".
+  
+  Use jq to parse further on the command line
 
-Use jq to parse further on the command line
+EXAMPLES
+
+Example:
+> ${basename(process.argv[1])} --option one=boolean,o --no-strict -- -otwo --three=four
+
+Example:
+> ${basename(process.argv[1])} --option one=string --no-strict -- --one two --three=four
 
 Example: 
-
 > config=$(jq -n '{"options": {"test": {"type": "string"}}, "strict": false}')
 > ${basename(process.argv[1])} --config "$config" -- --test "what a lovely day" | jq .
 
@@ -32,10 +43,6 @@ Example:
 
 Config Reference:
 https://nodejs.org/api/util.html#utilparseargsconfig
-
-Example:
-
-> ${basename(process.argv[1])} --option one=string --no-strict -- --one two --three=four
 
 `);
   process.exit(0)
@@ -78,16 +85,26 @@ try {
   if (!config) config = {}
   if (!config.options) config.options = {}
   for (const option of (values.option ?? [])) {
-    // TODO: check type is a real type, parse better
-    const [key, type] = option.split("=")
+    const [key, optionString] = option.split("=")
+    const [type, short] = optionString.split(",");
+    // check type is a real type
+    if (!['string', 'boolean'].includes(type))
+      throw new Error(`Type of ${key} is incorrect`)
     config.options[key] = { type }
+    // parse short option into config
+    if (!!short) {
+      if (short.length > 1) throw new Error(`Short option ${short} for option ${key} must be one character`)
+      config.options[key].short = short;
+    }
   }
   if (values.positional) config.allowPositionals = values.positional;
   if (values.negative) config.allowNegative = values.negative;
   if (values.strict) config.strict = values.strict;
 
-} catch {
+} catch (err) {
   process.stderr.write("Unable to parse your config\n")
+  if (typeof err === 'object' && !!err && 'message' in err)
+    process.stderr.write(`${err.message}\n`);
   process.exit(1)
 }
 
